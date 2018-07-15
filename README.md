edf2afb9e3022d41102a2345945803fc16a2cae3
raju <raju@MacBook-Pro.home>
2018-07-15 05:14:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+### Getting Started
+
+Checkout this repo, install dependencies, then start the gulp process with the following:
+
+```
+> cd react-proj
+> npm install
+> npm start
+```
+
+
+start editing src/index.js 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = ()=> {
+    return <div> This is my first react project </div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
